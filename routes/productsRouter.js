a9135aadf6f8a5b419648c2ebceae0532dc5539b
JarethWeir <148231602+JarethWeir@users.noreply.github.com>
2023-12-06 01:20:23 +0000

    Search backend setup

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('../models/Product'); // adjust path as necessary
+const { Op } = require('sequelize');
+const Product = require('../models/Product'); // Update the path as needed
 
-// Product search route
+// Search products route
 router.get('/search', async (req, res) => {
-  // ... search functionality ...
+    const searchTerm = req.query.term;
+    try {
+        const products = await Product.findAll({
+            where: {
+                [Op.or]: [
+                    {
+                        ProductName: {
+                            [Op.like]: `%${searchTerm}%` // Fuzzy search for product name
+                        }
+                    },
+                    {
+                        Category: {
+                            [Op.like]: `%${searchTerm}%` // Fuzzy search for category
+                        }
+                    }
+                ]
+            }
+        });
+        res.json(products);
+    } catch (error) {
+        res.status(500).send('Error searching for products: ' + error.message);
+    }
 });
 
 module.exports = router;
