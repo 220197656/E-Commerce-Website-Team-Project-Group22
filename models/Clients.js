073f70c79ca22c5b518277843d6f68def9dfc27f
JarethWeir <220070681@aston.ac.uk>
2023-12-11 22:10:00 +0000

    Update Clients.js

--- models/Clients.js
+++ models/Clients.js
@@ -1,23 +1,20 @@
-// models/Client.js
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../js/database');
-
+const sequelize = require('../js/database'); // Adjust path as necessary
 
 class Client extends Model {}
 
 Client.init({
-  ClientID: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  Username: { type: DataTypes.STRING, unique: true },
-  Password: DataTypes.STRING,
-  Email: { type: DataTypes.STRING, unique: true },
-  PhoneNumber: DataTypes.STRING,
-  FirstName: DataTypes.STRING,
-  LastName: DataTypes.STRING,
-  Address: DataTypes.TEXT
+    ClientID: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    Username: { type: DataTypes.STRING, unique: true },
+    Password: DataTypes.STRING,
+    Email: { type: DataTypes.STRING, unique: true },
+    PhoneNumber: DataTypes.STRING,
+    FirstName: DataTypes.STRING,
+    LastName: DataTypes.STRING
 }, {
-  sequelize,
-  modelName: 'Client',
-  timestamps: false
+    sequelize,
+    modelName: 'Client',
+    timestamps: false
 });
 
 module.exports = Client;
