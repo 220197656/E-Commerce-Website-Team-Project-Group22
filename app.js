a9d14c85f087a079381d21c1676717a9fb6ee724
JarethWeir <148231602+JarethWeir@users.noreply.github.com>
2023-12-11 20:35:40 +0000

    updated app.js + login.html event listener

--- app.js
+++ app.js
@@ -13,48 +13,58 @@ a user submits a login form on the index.html page, it sends a
 request to a route on the server defined in app.js.
 The server processes this request (e.g., authenticates the user) 
 and sends back a response, which could be a redirection to another 
-page, JSON data, etc. */
+page, JSON data, etc.
 
-//Import the associations file here
-
+helmet added for added security using https headers
+morgan request logger - sees incoming requests in dev env
+ */
 
 // Importing necessary modules
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
+const cors = require('cors'); 
+const helmet = require('helmet'); 
+const morgan = require('morgan');
+
+// Routers
 const productsRouter = require('./routes/productsRouter');
 const usersRouter = require('./routes/usersRouter');
 const ordersRouter = require('./routes/ordersRouter');
 const adminRouter = require('./routes/adminRouter');
 
-// Import the .env file
+// Load environment variables
 require('dotenv').config();
 
 // Initialize Express app
 const app = express();
 
-// Middlewares
+app.use(helmet()); 
+app.use(cors());
+app.use(morgan('dev'));
+
+// Middleware for parsing JSON
 app.use(express.json());
 
-// Passport Configuration
+// Passport Config
 require('./passportConfig')(passport);
 
 // Express session setup
 app.use(session({
-    secret: 'secret', // Replace with environment variable in production
+    secret: process.env.SESSION_SECRET || 'secret', // Use environment variable for final submission
     resave: true,
     saveUninitialized: true
 }));
 
-// Passport middleware
+// Passport middleware for authentication
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).send('Something broke!');
+    res.status(500).send('Something went wrong!');
 });
-
 
 // Using routers
 app.use('/api/products', productsRouter);
@@ -62,15 +72,16 @@ app.use('/api/users', usersRouter);
 app.use('/api/orders', ordersRouter);
 app.use('/api/admin', adminRouter);
 
-
-
-// Additional app configuration goes here
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+    res.status(404).send('Page not found');
+});
 
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-// Export app if needed for testing
+// Export app for testing purposes
 module.exports = app;
 
 
