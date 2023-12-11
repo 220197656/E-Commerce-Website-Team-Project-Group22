91eac3ebc26e5e433419d6e50363323023a99294
JarethWeir <148231602+JarethWeir@users.noreply.github.com>
2023-12-11 23:11:07 +0000

    Enable Sequelize Logging (Temporarily)

--- js/database.js
+++ js/database.js
@@ -1,7 +1,7 @@
 // file for connecting database with Sequelize
 // credentials stored as environmental variables for security purposes
 
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config({ path: '../../.env' });
 const { Sequelize } = require('sequelize');
 
 // Using environment variables for database credentials
@@ -12,7 +12,7 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST, // Database host
     dialect: 'mysql',
-    logging: false, // Optional: set to true if you want to see SQL log in the console
+    logging: true, // set  true  to see SQL log in console
   }
 );
 
