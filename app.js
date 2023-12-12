6d1dc3eab38a7b3df252db06dcafe5809ab4b926
JarethWeir <220070681@aston.ac.uk>
2023-12-12 11:24:19 +0000

    Update app.js

--- app.js
+++ app.js
@@ -21,15 +21,33 @@ morgan request logger - sees incoming requests in dev env
 // Importing necessary modules
 const express = require('express');
 const session = require('express-session');
-//const passport = require('passport'); Commented out all passport 
 const cors = require('cors'); 
 const helmet = require('helmet'); 
 const morgan = require('morgan');
 
+// Load environment variables
+require('dotenv').config();
+
+// Initialize Express app
+const app = express();
+
+// Set up middlewares
+app.use(helmet()); 
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json()); // Middleware for parsing JSON
+
+// Express session setup
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'secret', // Use environment variable for final submission
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Root route
 app.get('/', (req, res) => {
     res.send('Welcome to My Node.js App!');
-  });
-
+});
 
 // Routers
 const productsRouter = require('./routes/productsRouter');
@@ -39,39 +57,6 @@ const adminOrdersRouter = require('./routes/admin/orders');
 const adminProductsRouter = require('./routes/admin/products');
 const adminUsersRouter = require('./routes/admin/users');
 
-// Load environment variables
-require('dotenv').config();
-
-// Initialize Express app
-const app = express();
-
-app.use(helmet()); 
-app.use(cors());
-app.use(morgan('dev'));
-
-// Middleware for parsing JSON
-app.use(express.json());
-
-// Passport Config
-//require('./js/passportConfig')(passport); Commented out all passport 
-
-// Express session setup
-app.use(session({
-    secret: process.env.SESSION_SECRET || 'secret', // Use environment variable for final submission
-    resave: true,
-    saveUninitialized: true
-}));
-
-// Passport middleware for authentication
-//app.use(passport.initialize()); Commented out all passport 
-//app.use(passport.session()); Commented out all passport 
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
-});
-
 // Using routers
 app.use('/api/products', productsRouter);
 app.use('/api/users', usersRouter);
@@ -80,6 +65,12 @@ app.use('/admin/orders', adminOrdersRouter);
 app.use('/admin/products', adminProductsRouter);
 app.use('/admin/users', adminUsersRouter);
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
 // Catch 404 and forward to error handler
 app.use((req, res, next) => {
     res.status(404).send('Page not found');
@@ -87,9 +78,7 @@ app.use((req, res, next) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Export app for testing purposes
 module.exports = app;
