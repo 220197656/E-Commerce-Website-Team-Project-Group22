d43bc8f39cd58bfa50889f014d4ac11fe78623de
JarethWeir <148231602+JarethWeir@users.noreply.github.com>
2023-12-12 02:07:32 +0000

    Update database.js

--- js/database.js
+++ js/database.js
@@ -1,19 +1,27 @@
-// file for connecting database with Sequelize
-// credentials stored as environmental variables for security purposes
-
-require('dotenv').config({ path: '../../.env' });
+// database.js
 const { Sequelize } = require('sequelize');
+
+console.log('Initializing Sequelize'); // Debug code
 
 // Using environment variables for database credentials
 const sequelize = new Sequelize(
-  process.env.DB_NAME,    // Database name
-  process.env.DB_USER,    // Database username
-  process.env.DB_PASSWORD,  // Database password
+  process.env.DB_NAME,    
+  process.env.DB_USER,    
+  process.env.DB_PASSWORD,  
   {
     host: process.env.DB_HOST, // Database host
     dialect: 'mysql',
-    logging: console.log, // set  true  to see SQL log in console
+    logging: console.log, // Enable logging for SQL queries
   }
 );
+
+// Test the database connection
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection to the database has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = sequelize;
