dafd3e159bf226ab5483dc45cf7c78b0c46faf5b
JarethWeir <220070681@aston.ac.uk>
2023-12-12 11:44:36 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-/*
-Description of this file:
-
-app.js is  main server file. 
-It's responsible for setting up  server, defining routes, handling 
-requests, and integrating middleware (like Passport for authentication).
-It acts as the entry point to  backend application. When running the
-Node.js server, it's app.js that gets executed. */
-
-/*The frontend (HTML, CSS, JavaScript) interacts with the backend 
-(Node.js/Express server/ sequelize) through HTTP requests. For instance, when 
-a user submits a login form on the index.html page, it sends a 
-request to a route on the server defined in app.js.
-The server processes this request (e.g., authenticates the user) 
-and sends back a response, which could be a redirection to another 
-page, JSON data, etc.
-
-helmet added for added security using https headers
-morgan request logger - sees incoming requests in dev env
- */
 // Importing necessary modules
 const express = require('express');
 const session = require('express-session');
@@ -82,16 +62,3 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Export app for testing purposes
 module.exports = app;
-
-
-
-
-/*
-
-include admin routes 
-const adminOrdersRouter = require('./routes/admin/orders');
-const adminProductsRouter = require('./routes/admin/products');
-const adminUsersRouter = require('./routes/admin/users');
-
-
-*/
