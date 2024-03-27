e55a583cf013ad151606ac31437a90c5c7ca9f80
JarethWeir <220070681@aston.ac.uk>
2024-03-27 13:41:40 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -105,9 +105,9 @@ Route::get('search', function () {
     return view('search');
 });
 
-Route::get('checkout', function () {
-    return view('checkout');
-});
+// Route::get('checkout', function () {
+//     return view('checkout');
+// });
 
 Route::get('/search', [SearchController::class, 'index'])->name('search.index');
 Route::get('/search-results', [SearchController::class, 'searchResults'])->name('search.results');
